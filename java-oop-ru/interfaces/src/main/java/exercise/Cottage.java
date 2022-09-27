84c32ad2d76f02804bb517e765ff0fc573e16802
@hexlet/cli <support@hexlet.io>
2022-09-27 16:47:38 +0300

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/Cottage.java
@@ -0,0 +1,50 @@
+package exercise;
+
+// BEGIN
+public class Cottage implements Home{
+    public Double area;
+    public Integer floorCount;
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public void setFloorCount(int floorCount) {
+        this.floorCount = floorCount;
+    }
+
+    public int getFloorCount() {
+        return floorCount;
+    }
+
+    public Cottage() {}
+
+    public Cottage(double area, int floorCount) {
+        this.area = area;
+        this.floorCount = floorCount;
+    }
+
+    @Override
+    public double getArea() {
+        return area;
+    }
+
+    @Override
+    public int compareTo(Home another) {
+        int comparisonResult;
+        if (getArea() > another.getArea()) {
+            comparisonResult = 1;
+        } else if (getArea() < another.getArea()) {
+            comparisonResult = -1;
+        } else {
+            comparisonResult = 0;
+        }
+        return comparisonResult;
+    }
+
+    @Override
+    public String toString() {
+        return getFloorCount() + " этажный коттедж площадью " + getArea() + " метров";
+    }
+}
+// END
